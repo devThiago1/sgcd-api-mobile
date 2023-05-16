237ef442febf1c54c1b7b70d4fa825b0a30d037d
Thiago Calmon <thiago.calmon4@gmail.com>
2023-05-16 10:31:12 -0300

    database changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,13 +3,14 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import { User } from "./entities/User";
 import { Adress } from "./entities/Adress";
+import { Abrigos } from "./entities/Abrigo";
 
 const DB_PORT = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     url:process.env.DB_URL,
-    entities: [User, Adress],
+    entities: [User, Adress, Abrigos],
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
     synchronize: false,
 })
