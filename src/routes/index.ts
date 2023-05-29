b0e139dd3914fcdc846ceb05c9c18b9474c7f727
Thiago Calmon <thiago.calmon4@gmail.com>
2023-05-28 23:36:08 -0300

    posts 2.0

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { userLogin } from "../controller/userLogin";
+import { PostManager } from "../controller/PostManager";
 
 
 
@@ -8,6 +9,8 @@ const routes = Router();
 
 routes.post('/login', new userLogin().verfUser);
 routes.post('/verfToken', new userLogin().verfToken);
+routes.post('/insertPost', new PostManager().insertPost);
+routes.get('/getPosts', new PostManager().getPosts);
 
 
 export default routes;
