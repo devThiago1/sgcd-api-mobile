4697a4d76b5569cb49a718c7f032a615f7c298f6
Thiago Calmon <thiago.calmon4@gmail.com>
2023-05-31 08:58:41 -0300

    resolved

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,7 @@ const routes = Router();
 routes.post('/login', new userLogin().verfUser);
 routes.post('/verfToken', new userLogin().verfToken);
 routes.post('/insertPost', new PostManager().insertPost);
+routes.post('/updateUser', new userLogin().updateUser);
 routes.get('/getPosts', new PostManager().getPosts);
 routes.get('/getAbrigos', new AbrigoManager().getAbrigos);
 
